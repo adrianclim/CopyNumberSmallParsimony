b491df4d6c43c7556fcfc43186196faffd13b40a
adrianlim <aclim@sfu.ca>
2016-11-30 16:48:45 -0800

    Add backtracking function and printing helper function

--- copyNumberParsimonyTree.py
+++ copyNumberParsimonyTree.py
@@ -1,3 +1,5 @@
+import json
+
 def calculateParsimonyScore(rootNode):
     parsimonyScore = 0
 
@@ -33,25 +35,60 @@ def calculateParsimonyScore(rootNode):
 
     return parsimonyScore
 
+def backtrack(rootNode):
+    if 'parent' not in rootNode:
+        rootNode['backtrackState'] = list(rootNode['state'])[0]
+
+    else:
+        if rootNode['parent']['backtrackState'] in rootNode['state']:
+            rootNode['backtrackState'] = rootNode['parent']['backtrackState']
+
+        else:
+            rootNode['backtrackState'] = rootNode['state'][0]
+
+    if 'left' in rootNode:
+        backtrack(rootNode['left'])
+
+    if 'right' in rootNode:
+        backtrack(rootNode['right'])
+
+    return rootNode
+
+def printTree(rootNode, id):
+    #Prints the tree in post order
+
+    if 'left' in rootNode:
+        printTree(rootNode['left'], "LEFT")
+
+    if 'right' in rootNode:
+        printTree(rootNode['right'], "RIGHT")
+
+    print("BacktrackState: " + str(rootNode['backtrackState']))
 
 def testCalculateParsimonyScore():
     rootNode = {}
-    rootNode['left'] = {'state': [4]}
-    rootNode['right'] = {'state': [4,5]}
+    rootNode['left'] = {'state': [4], 'parent':rootNode}
+    rootNode['right'] = {'state': [5], 'parent': rootNode}
     print(calculateParsimonyScore(rootNode))
+    return rootNode
 
 def testCalculateParsimonyScore3Levels():
     rootNode = {}
-    rootNode['left'] = {}
-    rootNode['right'] = {}
+    rootNode['left'] = {'parent':rootNode}
+    rootNode['right'] = {'parent':rootNode}
 
-    rootNode['left']['left'] = {'state': [6]}
-    rootNode['left']['right'] = {'state': [7]}
-    rootNode['right']['left'] = {'state': [7]}
-    rootNode['right']['right'] = {'state': [8]}
+    rootNode['left']['left'] = {'state': [6], 'parent':rootNode['left']}
+    rootNode['left']['right'] = {'state': [7], 'parent':rootNode['left']}
+    rootNode['right']['left'] = {'state': [7], 'parent':rootNode['right']}
+    rootNode['right']['right'] = {'state': [8], 'parent':rootNode['right']}
 
     print(calculateParsimonyScore(rootNode))
+    return rootNode
+
 
 if __name__ == "__main__":
-    testCalculateParsimonyScore3Levels()
+    rootNode = testCalculateParsimonyScore3Levels()
+    rootNode = backtrack(rootNode)
+    printTree(rootNode, "ROOT")
+
 
